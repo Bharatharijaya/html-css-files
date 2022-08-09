62e0ee63110049f4c63a2a0f19ea7aaaf1a40285
Bharatharijaya <110047448+Bharatharijaya@users.noreply.github.com>
2022-08-09 19:20:17 +0530

    Add files via upload

---
+++ dhoni.js
@@ -0,0 +1,25 @@
+
+	
+function show() {
+
+    
+    document.getElementById('image')
+            .style.display = "block";
+
+    document.getElementById('btnID')
+            .style.display = "none";
+}
+function change()
+{
+    document.getElementById("msd").innerHTML = "Dhoni is one of Finest Wicketkeeper "
+    document.getElementsByTagName("p")[0].innerHTML = "Dhoni is one of world cup winning captian";
+}
+
+function myFunction() {
+    const elements = document.getElementsByClassName("MSD")[0];
+    elements.getElementsByClassName("type")[0].style.fontSize = "50px";
+    
+    
+}
+
+    
